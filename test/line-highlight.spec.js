06c8418773febbc6a9bccc1c09b46823fa08498e
Yuto Kojima <Kojima@Yuto-no-MacBook-Pro.local>
2017-05-10 21:56:53 +0900

    add languages option.

--- test/line-highlight.spec.js
+++ test/line-highlight.spec.js
@@ -73,14 +73,39 @@ export class HelloWorld extends Vue {
 
         });
 
-        it('div.line-highlight should be appended to pre element', () => {
+        it('data-line="9", one div.line-highlight should be appended', () => {
             target(env);
             expect(env.$element.parent().children('div.line-highlight').length).to.equals(1);
         });
 
-        it('div.line-highlight should have style top:xxx', () => {
+        it('data-line="9", div.line-highlight should have style top:216px', () => {
             target(env);
             expect(env.$element.parent().children('div.line-highlight').css('top')).to.equals('216px');
+        });
+
+        it('data-line="2-3,9", two div.line-highlight should be appended', () => {
+            highlightedHtml =
+`<pre class="line-numbers" data-line="2-3,9">
+    <code class="language-typescrip">${highlightedCode}
+    </code>
+</pre>`;
+            $ = cheerio.load(highlightedHtml);
+            env.$element = $('code');
+            target(env);
+            expect(env.$element.parent().children('div.line-highlight').length).to.equals(2);
+        });
+
+        it('data-line="2-3,9", two div.line-highlight have style top:48px and top:216px', () => {
+            highlightedHtml =
+`<pre class="line-numbers" data-line="2-3,9">
+    <code class="language-typescrip">${highlightedCode}
+    </code>
+</pre>`;
+            $ = cheerio.load(highlightedHtml);
+            env.$element = $('code');
+            target(env);
+            expect(env.$element.parent().children('div.line-highlight').eq(0).css('top')).to.equals('48px');
+            expect(env.$element.parent().children('div.line-highlight').eq(1).css('top')).to.equals('216px');
         });
 
     });
